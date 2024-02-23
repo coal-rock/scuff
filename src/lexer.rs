23f7b16e29afc90b7f560ccd5057532c285a8dd4
nikki-sh <139382119+nikki-sh@users.noreply.github.com>
2024-02-22 22:44:38 -0500

    woah we have a lexer now

---
+++ src/lexer.rs
@@ -0,0 +1,319 @@
+#[derive(Debug, Clone, PartialEq)]
+pub enum TokenType {
+    LeftParen,
+    RightParen,
+    LeftBracket,
+    RightBracket,
+    LeftBrace,
+    RightBrace,
+    Comma,
+    Dot,
+    Minus,
+    Plus,
+    Semicolon,
+    Colon,
+    Slash,
+    Star,
+    Caret,
+
+    Bang,
+    Equal,
+
+    EqualEqual,
+    BangEqual,
+
+    Greater,
+    Less,
+
+    GreaterEqual,
+    LessEqual,
+
+    Arrow,
+
+    Ident(String),
+    String(String),
+    Number(f64),
+
+    True,
+    False,
+
+    If,
+    Else,
+    And,
+    Or,
+    Break,
+    Continue,
+    Return,
+    Let,
+
+    NumberType,
+    StringType,
+    BoolType,
+    TableType,
+    VoidType,
+
+    While,
+    For,
+
+    Function,
+
+    EOF,
+}
+
+#[derive(Debug, Clone)]
+pub struct Token {
+    pub token_type: TokenType,
+    pub line: usize,
+}
+
+impl Token {
+    pub fn new(token_type: TokenType, line: usize) -> Token {
+        Token { token_type, line }
+    }
+
+    pub fn to_string(&self) -> String {
+        format!("{:?}", self.token_type)
+    }
+}
+
+pub struct Lexer {
+    source: String,
+    tokens: Vec<Token>,
+    start: usize,
+    current: usize,
+    line: usize,
+}
+
+impl Lexer {
+    pub fn new(source: String) -> Lexer {
+        Lexer {
+            source,
+            tokens: Vec::new(),
+            start: 0,
+            current: 0,
+            line: 1,
+        }
+    }
+
+    fn is_at_end(&self) -> bool {
+        self.current >= self.source.len()
+    }
+
+    // rust has these in-built, but we define them slightly
+    // differently
+    fn is_digit(&self, character: char) -> bool {
+        match character {
+            '0'..='9' => true,
+            _ => false,
+        }
+    }
+
+    fn is_alpha(&self, character: char) -> bool {
+        match character {
+            'a'..='z' | 'A'..='Z' | '_' => true,
+            _ => false,
+        }
+    }
+
+    fn is_alpha_numeric(&self, character: char) -> bool {
+        self.is_digit(character) || self.is_alpha(character)
+    }
+
+    fn advance(&mut self) -> char {
+        let char = self
+            .source
+            .chars()
+            .collect::<Vec<char>>()
+            .get(self.current)
+            .unwrap_or(&'\0')
+            .clone();
+
+        self.current += 1;
+
+        char
+    }
+
+    fn peek(&self) -> char {
+        let chars = self.source.chars().collect::<Vec<char>>();
+        let char = chars.get(self.current);
+
+        if self.is_at_end() {
+            return '\0';
+        }
+
+        return char.unwrap().clone();
+    }
+
+    fn peek_next(&self) -> char {
+        let chars = self.source.chars().collect::<Vec<char>>();
+        let char = chars.get(self.current + 1);
+
+        if self.is_at_end() {
+            return '\0';
+        }
+
+        return char.unwrap().clone();
+    }
+
+    fn compare(&mut self, expected: char) -> bool {
+        if self.is_at_end() {
+            return false;
+        }
+
+        if self.peek() != expected {
+            return false;
+        }
+
+        self.current += 1;
+        true
+    }
+
+    // Basically ternary
+    fn add_token_cond(&mut self, expected: char, matches: TokenType, fails: TokenType) {
+        if self.compare(expected) {
+            self.add_token(matches);
+        } else {
+            self.add_token(fails);
+        }
+    }
+
+    fn add_token(&mut self, token_type: TokenType) {
+        let string: String = self.source[self.start..self.current].to_string();
+
+        self.tokens.push(Token::new(token_type, self.line));
+    }
+
+    fn string_to_keyword(&self, string: String) -> Option<TokenType> {
+        match string.as_str() {
+            "if" => Some(TokenType::If),
+            "else" => Some(TokenType::Else),
+            "and" => Some(TokenType::And),
+            "or" => Some(TokenType::Or),
+            "break" => Some(TokenType::Break),
+            "continue" => Some(TokenType::Continue),
+            "return" => Some(TokenType::Return),
+            "while" => Some(TokenType::While),
+            "for" => Some(TokenType::For),
+            "function" => Some(TokenType::Function),
+            "true" => Some(TokenType::True),
+            "false" => Some(TokenType::False),
+            "let" => Some(TokenType::Let),
+            "number" => Some(TokenType::NumberType),
+            "string" => Some(TokenType::StringType),
+            "bool" => Some(TokenType::BoolType),
+            "table" => Some(TokenType::TableType),
+            "void" => Some(TokenType::VoidType),
+            _ => None,
+        }
+    }
+
+    fn lex_string(&mut self) {
+        while self.peek() != '"' && !self.is_at_end() {
+            if self.peek() == '\n' {
+                self.line += 1
+            };
+            self.advance();
+        }
+
+        if self.is_at_end() {
+            todo!();
+            // error(self.line, "Unterminted string.".to_string());
+        }
+
+        self.advance();
+
+        let string: String = self.source[self.start + 1..self.current - 1].to_string();
+        self.add_token(TokenType::String(string));
+    }
+
+    // this might be hacky, maybe in the future
+    // we should handle number as ints instead of floats
+    fn lex_number(&mut self) {
+        while self.is_digit(self.peek()) {
+            self.advance();
+        }
+
+        if self.peek() == '.' && self.is_digit(self.peek_next()) {
+            self.advance();
+
+            while self.is_digit(self.peek()) {
+                self.advance();
+            }
+        }
+
+        let number = self.source[self.start..self.current].to_string();
+        let number: f64 = number.parse().unwrap();
+
+        self.add_token(TokenType::Number(number));
+    }
+
+    fn lex_identifier(&mut self) {
+        while self.is_alpha_numeric(self.peek()) {
+            self.advance();
+        }
+
+        let string = self.source[self.start..self.current].to_string();
+        let token = self.string_to_keyword(string.clone());
+
+        if token.is_none() {
+            self.add_token(TokenType::Ident(string));
+        } else {
+            self.add_token(token.unwrap());
+        }
+    }
+
+    pub fn lex_token(&mut self) {
+        let char = self.advance();
+
+        match char {
+            '(' => self.add_token(TokenType::LeftParen),
+            ')' => self.add_token(TokenType::RightParen),
+            '[' => self.add_token(TokenType::LeftBracket),
+            ']' => self.add_token(TokenType::RightBracket),
+            '{' => self.add_token(TokenType::LeftBrace),
+            '}' => self.add_token(TokenType::RightBrace),
+            ',' => self.add_token(TokenType::Comma),
+            '.' => self.add_token(TokenType::Dot),
+            '+' => self.add_token(TokenType::Plus),
+            ';' => self.add_token(TokenType::Semicolon),
+            '*' => self.add_token(TokenType::Star),
+            '^' => self.add_token(TokenType::Caret),
+            ':' => self.add_token(TokenType::Colon),
+            '!' => self.add_token_cond('=', TokenType::BangEqual, TokenType::Bang),
+            '=' => self.add_token_cond('=', TokenType::EqualEqual, TokenType::Equal),
+            '<' => self.add_token_cond('=', TokenType::LessEqual, TokenType::Less),
+            '>' => self.add_token_cond('=', TokenType::GreaterEqual, TokenType::Greater),
+            '-' => self.add_token_cond('>', TokenType::Arrow, TokenType::Minus),
+            '/' => {
+                if self.compare('/') {
+                    while self.peek() != '\n' && !self.is_at_end() {
+                        self.advance();
+                    }
+                } else {
+                    self.add_token(TokenType::Slash);
+                }
+            }
+            '"' => self.lex_string(),
+            '\t' | ' ' => {}
+            '\n' => self.line += 1,
+            '0'..='9' => self.lex_number(),
+            'a'..='z' | 'A'..='Z' | '_' => self.lex_identifier(),
+            _ => {
+                if !self.peek().is_ascii_whitespace() {
+                    todo!();
+                    //error(self.line, format!("Unexpected character: {:?}", char))
+                }
+            }
+        }
+    }
+
+    pub fn lex(&mut self) -> Vec<Token> {
+        while !self.is_at_end() {
+            self.start = self.current;
+            self.lex_token();
+        }
+
+        self.add_token(TokenType::EOF);
+        return self.tokens.clone();
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+mod lexer;
+
+use crate::lexer::Lexer;
+use std::{env, fs::read_to_string};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    assert!(args.len() > 1, "Expected file path.");
+
+    let file_contents = read_to_string(args[1].clone()).expect("Could not read file");
+    let mut lexer = Lexer::new(file_contents);
+
+    let tokens = lexer.lex();
+    println!("{:#?}", tokens);
 }
