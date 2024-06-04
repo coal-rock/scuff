29ed6a056e701ae3ff341ae5ff73f9f5005a89a2
nikki-sh <139382119+nikki-sh@users.noreply.github.com>
2024-06-03 22:35:45 -0400

    `makefile.rs` now reads content of files

--- src/main.rs
+++ src/main.rs
@@ -6,76 +6,26 @@ mod parser;
 mod project;
 mod token;
 
-use crate::{compiler::Compiler, lexer::Lexer, makefile::Makefile, parser::Parser};
-use std::{env, fs::read_to_string};
+use crate::{compiler::Compiler, lexer::Lexer, makefile::MakefileData, parser::Parser};
+use std::{env, fs::read_to_string, path::PathBuf};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     assert!(args.len() > 1, "Expected file path.");
 
-    let file_contents = read_to_string(args[1].clone()).expect("Could not read file");
-
-    let mut lexer = Lexer::new(file_contents);
-    let tokens = lexer.lex();
-
-    let mut parser = Parser::new(tokens.clone());
-    let ast = parser.parse();
-
-    println!("{:#?}", &ast);
-
-    let mut compiler = Compiler::new(ast);
-    let project = compiler.compile();
-
-    println!("{}", serde_json::to_string_pretty(project).unwrap());
-
-    let makefile: Makefile = toml::from_str(
-        r#"
-            project_name = "Example Project"
-            extensions = [
-                "Pen", "Music", "VideoSensing", "Text2Speech", "Translate",
-                "Makeymakey", "Microbit", "EV3", "Boost", "Wedo2", "Gdxfor"
-            ]
-
-            [[stage]]
-            name = "Stage 1"
-            script = "scripts/stage1.sb3"
-            backdrops = [
-                { name = "Backdrop 1", path = "assets/backdrops/backdrop1.png" },
-                { name = "Backdrop 2", path = "assets/backdrops/backdrop2.png" }
-            ]
-            sounds = [
-                { name = "Sound 1", path = "assets/sounds/stage1/sound1.wav" },
-                { name = "Sound 2", path = "assets/sounds/stage1/sound2.wav" }
-            ]
-
-            [[sprite]]
-            name = "Sprite 1"
-            script = "scripts/sprite1.sb3"
-            costumes = [
-                { name = "Costume 1", path = "assets/costumes/sprite1/costume1.png" },
-                { name = "Costume 2", path = "assets/costumes/sprite1/costume2.png" }
-            ]
-            sounds = [
-                { name = "Sound 1", path = "assets/sounds/sprite1/sound1.wav" },
-                { name = "Sound 2", path = "assets/sounds/sprite1/sound2.wav" }
-            ]
-
-            [[sprite]]
-            name = "Sprite 2"
-            script = "scripts/sprite2.sb3"
-            costumes = [
-                { name = "Costume 1", path = "assets/costumes/sprite2/costume1.png" },
-                { name = "Costume 2", path = "assets/costumes/sprite2/costume2.png" }
-            ]
-            sounds = [
-                { name = "Sound 1", path = "assets/sounds/sprite2/sound1.wav" },
-                { name = "Sound 2", path = "assets/sounds/sprite2/sound2.wav" }
-            ]
-        "#,
-    )
-    .unwrap();
-
+    let makefile = MakefileData::parse(args[1].clone().into());
     println!("{:#?}", makefile);
 
-    // println!("{}", toml::to_string(&makefile).unwrap());
+    // let mut lexer = Lexer::new(file_contents);
+    // let tokens = lexer.lex();
+    //
+    // let mut parser = Parser::new(tokens.clone());
+    // let ast = parser.parse();
+    //
+    // println!("{:#?}", &ast);
+    //
+    // let mut compiler = Compiler::new(ast);
+    // let project = compiler.compile();
+    //
+    // println!("{}", serde_json::to_string_pretty(project).unwrap());
 }
--- src/makefile.rs
+++ src/makefile.rs
@@ -1,5 +1,8 @@
 use serde::{Deserialize, Serialize};
-use std::{collections::HashMap, path::PathBuf};
+use std::{
+    fs::{read, read_to_string},
+    path::{Path, PathBuf},
+};
 
 #[derive(Deserialize, Serialize, Debug)]
 pub enum Extension {
@@ -17,13 +20,13 @@ pub enum Extension {
 }
 
 #[derive(Deserialize, Serialize, Debug)]
-pub struct Asset {
+struct Asset {
     pub name: String,
     pub path: PathBuf,
 }
 
 #[derive(Deserialize, Serialize, Debug)]
-pub struct Stage {
+struct Stage {
     pub name: String,
     pub script: PathBuf,
     pub backdrops: Vec<Asset>,
@@ -31,7 +34,7 @@ pub struct Stage {
 }
 
 #[derive(Deserialize, Serialize, Debug)]
-pub struct Sprite {
+struct Sprite {
     pub name: String,
     pub script: PathBuf,
     pub costumes: Vec<Asset>,
@@ -39,9 +42,94 @@ pub struct Sprite {
 }
 
 #[derive(Deserialize, Serialize, Debug)]
-pub struct Makefile {
+struct Makefile {
     pub project_name: String,
     pub stage: Vec<Stage>,
     pub sprite: Vec<Sprite>,
     pub extensions: Vec<Extension>,
+}
+
+#[derive(Debug)]
+pub struct AssetData {
+    name: String,
+    content: Vec<u8>,
+}
+
+#[derive(Debug)]
+pub struct StageData {
+    pub name: String,
+    pub script: String,
+    pub backdrops: Vec<AssetData>,
+    pub sounds: Vec<AssetData>,
+}
+
+#[derive(Debug)]
+pub struct SpriteData {
+    pub name: String,
+    pub script: String,
+    pub costumes: Vec<AssetData>,
+    pub sounds: Vec<AssetData>,
+}
+
+// like Makefile, but contains data from each file listed instead of paths
+#[derive(Debug)]
+pub struct MakefileData {
+    pub project_name: String,
+    pub stage: Vec<StageData>,
+    pub sprite: Vec<SpriteData>,
+    pub extensions: Vec<Extension>,
+}
+
+impl MakefileData {
+    pub fn parse(makefile_path: PathBuf) -> MakefileData {
+        let makefile: Makefile =
+            toml::from_str(&read_to_string(makefile_path.clone()).unwrap()).unwrap();
+
+        let project_path = makefile_path.clone();
+        let project_path = project_path.parent().unwrap();
+
+        let mut stages: Vec<StageData> = vec![];
+
+        for stage in makefile.stage {
+            stages.push(StageData {
+                name: stage.name,
+                script: read_to_string(MakefileData::get_path(project_path, stage.script)).unwrap(),
+                backdrops: MakefileData::read_assets(project_path, stage.backdrops),
+                sounds: MakefileData::read_assets(project_path, stage.sounds),
+            });
+        }
+
+        let mut sprites: Vec<SpriteData> = vec![];
+
+        for sprite in makefile.sprite {
+            sprites.push(SpriteData {
+                name: sprite.name,
+                script: read_to_string(MakefileData::get_path(project_path, sprite.script))
+                    .unwrap(),
+                costumes: MakefileData::read_assets(project_path, sprite.costumes),
+                sounds: MakefileData::read_assets(project_path, sprite.sounds),
+            });
+        }
+
+        MakefileData {
+            project_name: makefile.project_name,
+            stage: stages,
+            sprite: sprites,
+            extensions: makefile.extensions,
+        }
+    }
+
+    fn get_path(project_path: &Path, file_path: PathBuf) -> String {
+        project_path.join(file_path).to_str().unwrap().to_string()
+    }
+
+    fn read_assets(project_path: &Path, assets: Vec<Asset>) -> Vec<AssetData> {
+        assets
+            .iter()
+            .map(|asset| AssetData {
+                name: asset.name.clone(),
+                content: read(MakefileData::get_path(project_path, asset.path.clone())).unwrap(),
+            })
+            .collect()
+    }
 }
